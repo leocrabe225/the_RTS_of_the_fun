51ca472aa82cccada5b095428487d6d9f0aaa5ef
Leo CALENDINI <lcalendi@e1r13p12.42.fr>
2020-10-16 18:01:18 +0200

    Ready to start coding some stuff

--- rts.c
+++ rts.c
@@ -1,31 +1,43 @@
 #include "mlx.h"
+#include <stdlib.h>
 
 #define WIDTH 500
 #define HEIGHT 500
 
+typedef struct	s_main
+{
+	void *mlx;
+	void *window;
+	void *mlx_image;
+	void *img;
+	void ***object_map;
+	int line_size;
+}				t_main;
 
+int loop_hook(t_main *data)
+{
+	mlx_put_image_to_window(data->mlx, data->window, data->mlx_image, 0, 0);
+	return (0);
+}
 
 int main()
 {
-	void	 *mlx;
-	void	 *window;
-	void	*mlx_image;
-	void	*img;
-	void 	***object_map
+	t_main data;
 	int		garbage;
-	int		line_size;
 	int		loop;
 
-	mlx = mlx_init();
-	window = mlx_new_window(mlx, WIDTH, HEIGHT, "A basic RTS");
-	mlx_image = mlx_new_image(mlx, WIDTH, HEIGHT);
-	img = mlx_get_data_addr(mlx_image, &garbage, &line_size, &garbage);
-	object_map = malloc(sizeof(object_map) * (HEIGHT + 1));
-	object_map[HEIGHT] = 0;
+	data.mlx = mlx_init();
+	data.window = mlx_new_window(data.mlx, WIDTH, HEIGHT, "A basic RTS");
+	data.mlx_image = mlx_new_image(data.mlx, WIDTH, HEIGHT);
+	data.img = mlx_get_data_addr(data.mlx_image, &garbage, &data.line_size, &garbage);
+	data.object_map = malloc(sizeof(data.object_map) * (HEIGHT + 1));
+	data.object_map[HEIGHT] = 0;
 	loop = 0;
 	while (loop != HEIGHT)
 	{
-		object_map[loop] = malloc(sizeof(*object_map) * (WIDTH + 1));
+		data.object_map[loop] = malloc(sizeof(*data.object_map) * (WIDTH + 1));
 		loop++;
-	} 
+	}
+	mlx_loop_hook(data.mlx, *mlx_loop, &data);
+	mlx_loop(data.mlx);
 }
